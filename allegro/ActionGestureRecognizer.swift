a10ff6872852f55d96fe16a0362ee9f528677701
Brian Tiger Chow <brian.holderchow@gmail.com>
2017-01-13 21:02:44 -0800

    fix(Gestures): only one of x and y needs to be different to add a Point to array

--- allegro/ActionGestureRecognizer.swift
+++ allegro/ActionGestureRecognizer.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-// TODO(btc): move this somewhere appropriate
 enum ActionGesture: String {
     case note // tap
     case flat
@@ -24,18 +23,25 @@ protocol ActionGestureDelegate: class {
     func actionGestureRecognized(gesture: ActionGesture, at location: CGPoint)
 }
 
+// TODO(btc): Note that one obvious improvement is to subclass UIGestureRecognizer. That makes it so that clients don't
+// have to delegate their touches(Began|Moved|Ended) methods to this class. Instead, with the subclass, the UIKit framework
+// will be responsible for calling our methods and managing our state. However, this alternative limits us when it comes
+// time to report which action was recognized. The addTarget method on the subclass doesn't allow us to communicate which
+// action was recognized (as our ActionGestureDelegate protocol does). For this reason, I'm not convinced it's worth the
+// rewrite at this time. However, if this class introduces problems, let this note be a guide towards an alternative design
+// with slightly different constraints and benefits.
 class ActionGestureRecognizer {
 
     var delta: Double = 22
     var costMax = 1
-    
+
     private let swipeGestureRecognizer: DBPathRecognizer?
     private var rawPoints: [Int] = [Int]()
-    
+
     weak var view: UIView?
     weak var delegate: ActionGestureDelegate?
 
-    // TODO we should probably make it explicit that this class will add gesture recognizers to the view
+    // TODO we should probably make it explicit that this class will add gesture recognizers to the view.
     init(view: UIView) {
         self.view = view
         swipeGestureRecognizer = DBPathRecognizer(sliceCount: 8,
@@ -44,22 +50,22 @@ class ActionGestureRecognizer {
         setupTapGestures()
         setupSwipeGestures()
     }
-    
+
     private func setupTapGestures() {
         let doubleDot = UITapGestureRecognizer(target: self, action: #selector(tapped))
         doubleDot.numberOfTapsRequired = 3
         let dot = UITapGestureRecognizer(target: self, action: #selector(tapped))
         dot.numberOfTapsRequired = 2
         let note = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        
+
         dot.require(toFail: doubleDot)
         note.require(toFail: dot)
-        
+
         for gr in [doubleDot, dot, note] {
             view?.addGestureRecognizer(gr)
         }
     }
-    
+
     private func setupSwipeGestures() {
         let map: [ActionGesture : [Int]] = [
             .flat: [0,2],
@@ -73,11 +79,11 @@ class ActionGestureRecognizer {
             swipeGestureRecognizer?.addModel(p)
         }
     }
-    
+
     func reset() {
         rawPoints = []
     }
-    
+
     func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let t = touches.first, let view = view {
             reset()
@@ -86,19 +92,19 @@ class ActionGestureRecognizer {
             rawPoints.append(Int(location.y))
         }
     }
-    
+
     func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let t = touches.first, let view = view, rawPoints.count >= 2 {
             let location = t.location(in: view)
-            let xNotTheSame = rawPoints[rawPoints.count-2] != Int(location.x)
-            let yNotTheSame = rawPoints[rawPoints.count-1] != Int(location.y)
-            if (xNotTheSame && yNotTheSame) {
+            let xIsDifferent = rawPoints[rawPoints.count-2] != Int(location.x)
+            let yIsDifferent = rawPoints[rawPoints.count-1] != Int(location.y)
+            if (xIsDifferent || yIsDifferent) {
                 rawPoints.append(Int(location.x))
                 rawPoints.append(Int(location.y))
             }
         }
     }
-    
+
     func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         var path = Path()
         path.addPointFromRaw(rawPoints)
@@ -108,7 +114,6 @@ class ActionGestureRecognizer {
             let point = CGPoint(x: rawPoints[0], y: rawPoints[1])
             delegate?.actionGestureRecognized(gesture: type, at: point)
         }
-        // TODO notify delegate
     }
 
     @objc private func tapped(sender: UITapGestureRecognizer) {
