1aa3ac51dfb72313bb557cb7879be117e50cfada
Nikhil Lele <nik.lele@gmail.com>
2017-01-18 14:37:33 -0800

    for #38 basic attributes of Part, Measure, Note and some tests

--- allegro/Note.swift
+++ allegro/Note.swift
@@ -6,6 +6,23 @@
 //  Copyright © 2017 gigaunicorn. All rights reserved.
 //
 
-class Note {
 
+struct Note {
+    // TODO pitch (see #42)
+    // TODO duration (see #37)
+    
+    enum Accidental {
+        case natural    // unicode ♮
+        case sharp      // unicode ♯
+        case flat       // unicode ♭
+    }
+    let accidental: Note.Accidental
+    
+    // true if the Note is a rest
+    let rest: Bool
+    
+    init(accidental: Note.Accidental = .natural, rest: Bool = false) {
+        self.accidental = accidental
+        self.rest = rest
+    }
 }
