e6eed6235f4da48fb04bf548d32812f105c77f27
Nikhil Lele <nik.lele@gmail.com>
2017-01-18 14:37:33 -0800

    for #38 basic attributes of Part, Measure, Note and some tests

--- allegro/Part.swift
+++ allegro/Part.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017 gigaunicorn. All rights reserved.
 //
 
-import Foundation
+struct Part {
+    
+    // beats per minute (bpm) eg. 120
+    let tempo: Int
+    
+    let composer: String
+    let title: String
+    let comment: String
+    
+    // ordered list of measures in the piece
+    var measures: [Measure]
+    
+    // initialize with standard 120 bpm and 1 empty measure
+    init(tempo: Int = 120, composer: String = "", title: String = "",
+         comment: String = "", measures: [Measure] = [Measure]()) {
+        self.tempo = tempo
+        self.composer = composer
+        self.title = title
+        self.comment = comment
+        
+        self.measures = measures
+        self.measures.append(Measure())
+        
+    }
+    
+}
