22dfe1af36d3879a7e0f065e64e00065321bfb05
Qingping He <qhe@stanford.edu>
2017-01-18 17:38:44 -0800

    draws a half note

--- allegro/Note.swift
+++ allegro/Note.swift
@@ -7,5 +7,13 @@
 //
 
 class Note {
+    // Quick n dirty pitch for now.
+    // 0 is the center of the bars.
+    // Every increment by 1 moves up half staff height
+    // -1 moves it down
+    var pitch = 0
     
+    init(pitch: Int) {
+        self.pitch = pitch
+    }
 }
