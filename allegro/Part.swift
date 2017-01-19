fdab28e3f2570065c0ffe9736069ad31c5673077
Nikhil Lele <nik.lele@gmail.com>
2017-01-19 09:13:18 -0800

    integrate changes from PR #43

--- allegro/Part.swift
+++ allegro/Part.swift
@@ -9,24 +9,17 @@
 struct Part {
     
     // beats per minute (bpm) eg. 120
-    let tempo: Int
+    var tempo: Int = 120
     
-    let composer: String
-    let title: String
-    let comment: String
+    var composer: String = ""
+    var title: String = ""
+    var comment: String = ""
     
     // ordered list of measures in the piece
-    var measures: [Measure]
+    fileprivate var measures: [Measure] = [Measure()]
     
-    // initialize with standard 120 bpm and 1 empty measure
-    init(tempo: Int = 120, composer: String = "", title: String = "",
-         comment: String = "", measures: [Measure] = [Measure]()) {
-        self.tempo = tempo
-        self.composer = composer
-        self.title = title
-        self.comment = comment
-        
-        self.measures = measures
+    // initialize with 1 empty measure
+    init() {
         self.measures.append(Measure())
         
     }
