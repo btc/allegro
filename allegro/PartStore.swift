a822c8c72941de43022df807cf090d8312be8c59
Brian Tiger Chow <brian.holderchow@gmail.com>
2017-01-21 16:04:08 -0800

    add PartStore

---
+++ allegro/PartStore.swift
@@ -0,0 +1,18 @@
+//
+//  PartStore.swift
+//  allegro
+//
+//  Created by Brian Tiger Chow on 1/21/17.
+//  Copyright © 2017 gigaunicorn. All rights reserved.
+//
+
+class PartStore {
+
+    let part: Part
+
+    init(part: Part) {
+        self.part = part
+    }
+
+    
+}
