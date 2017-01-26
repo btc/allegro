5a018264ea41f1e21b0b10c44ff4abce9abd3d52
Brian Tiger Chow <brian.holderchow@gmail.com>
2017-01-26 14:06:20 -0800

    debug: include Tweaks in debug mode only

--- allegro/AppDelegate.swift
+++ allegro/AppDelegate.swift
@@ -20,7 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = TweakWindow(frame: UIScreen.main.bounds, tweakStore: Tweaks.defaultStore)
+        if DEBUG {
+            window = TweakWindow(frame: UIScreen.main.bounds, tweakStore: Tweaks.defaultStore)
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+        }
         window?.rootViewController = RootNavigationViewController(rootViewController: HomeMenuViewController())
         window?.makeKeyAndVisible()
         Log.info?.message("App launched!")
