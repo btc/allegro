9ab9e1055b4e2d473ec87477b1e18f0cd75c4325
Brian Tiger Chow <brian.holderchow@gmail.com>
2017-01-26 19:29:35 -0800

    UI: add Montserrat font

--- allegro/Utils.swift
+++ allegro/Utils.swift
@@ -22,8 +22,8 @@ typealias Log = CleanroomLogger.Log // to avoid importing CleanroomLogger in eve
 let THE_GOLDEN_RATIO: CGFloat = 1.61803398875
 let DEFAULT_MARGIN_PTS: CGFloat = 22
 let DEFAULT_TAP_TARGET_SIZE: CGFloat = 60
-let DEFAULT_FONT_BOLD = "HelveticaNeue-Bold"
-let DEFAULT_FONT = "HelveticaNeue"
+let DEFAULT_FONT_BOLD = "Montserrat-Bold"
+let DEFAULT_FONT = "Montserrat-Regular"
 
 extension UIColor {
     static let allegroBlue = UIColor(red:0.68, green:0.92, blue:1.0, alpha:1.0)
