b82ea48dc9f7a58acd4faf2d7e0a8556a4d2445b
Brian Tiger Chow <brian.holderchow@gmail.com>
2017-01-30 14:09:35 -0800

    add RestView stub

---
+++ allegro/RestView.swift
@@ -0,0 +1,12 @@
+//
+//  RestView.swift
+//  allegro
+//
+//  Created by Brian Tiger Chow on 1/30/17.
+//  Copyright © 2017 gigaunicorn. All rights reserved.
+//
+
+import UIKit
+
+class RestView: UIView {
+}
