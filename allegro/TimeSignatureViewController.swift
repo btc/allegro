ac4dcc4aaeece88947a7d5b8811d0347f61814b4
Priyanka <ppsekhar@gmail.com>
2017-02-01 15:39:50 -0800

    started building picker view for timesignature selection screen

--- allegro/TimeSignatureViewController.swift
+++ allegro/TimeSignatureViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class TimeSignatureViewController: UIViewController {
+class TimeSignatureViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    
+    private let pickerData = [
+        ["2","3","4","6","8"],
+        ["2","3","4","6","8"]
+    ]
+
     private let backButton: UIButton = {
         let v = UIButton()
         v.backgroundColor = UIColor.allegroPurple
@@ -17,18 +23,23 @@ class TimeSignatureViewController: UIViewController {
         return v
     }()
     
+    var timeSigPickerView: UIPickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         
-        view.addSubview(backButton)
+        //view.addSubview(backButton)
+        view.addSubview(timeSigPickerView)
+        timeSigPickerView.delegate = self
+        timeSigPickerView.dataSource = self
         
         backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         
     }
     
     override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews() // NB: does nothing
+        super.viewDidLayoutSubviews()
         
         let parent = view.bounds
         let centerX = parent.width / 2
@@ -42,11 +53,44 @@ class TimeSignatureViewController: UIViewController {
                                             y: parent.height / 2 + DEFAULT_MARGIN_PTS,
                                             width: buttonW,
                                             height: buttonH)
+        timeSigPickerView.frame = CGRect(x:self.view.frame.size.width * 0.1,
+                                         y:self.view.frame.size.height * 0.1,
+                                         width: self.view.frame.size.width * 0.8,
+                                         height: self.view.frame.size.height * 0.8)
 
     }
     
     func backButtonTapped() {
         let _ = navigationController?.popViewController(animated: true)
     }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return 1
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_
+        pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int
+        ) -> Int {
+        return pickerData[component].count
+    }
+    
+    func pickerView(_
+        pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int
+        ) -> String? {
+        return pickerData[component][row]
+    }
+    
+
 
 }
