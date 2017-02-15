3f47f058e4cc944a82815ec34ebd6e651848ecd6
Kevin <kevin@kevincoelho.com>
2017-02-15 14:29:49 -0800

    Added basic implementation for Triplet object. Minimal testing / error checking -> needs code added to note model to complete Triplet implementation

--- allegro/Triplet.swift
+++ allegro/Triplet.swift
@@ -5,7 +5,16 @@
 //  Created by Brian Tiger Chow on 1/16/17.
 //  Copyright © 2017 gigaunicorn. All rights reserved.
 //
+import Rational
 
 class Triplet {
-    
+    let notes: [Note]   // keep track of notes in the triplet
+    let realDuration: Rational  //duration of each note in the triplet
+    init?(note1: Note, note2: Note, note3: Note, nominalDuration: Rational) {
+        guard(note1.duration == note2.duration && note2.duration == note3.duration) else {
+            return nil
+        }
+        self.notes = [Note](arrayLiteral: note1, note2, note3)
+        self.realDuration = nominalDuration * 2/3;
+    }
 }
