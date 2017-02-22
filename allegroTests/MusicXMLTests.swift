6f0915a382024f8d033a4e7211dfb33ef713292d
Nikhil Lele <nik.lele@gmail.com>
2017-02-22 13:10:24 -0800

    fix test that was not updating store correctly

--- allegroTests/MusicXMLTests.swift
+++ allegroTests/MusicXMLTests.swift
@@ -26,13 +26,11 @@ class musicXMLTests: XCTestCase {
     func testParse() {
         let part = Part()
         let store = PartStore(part: part)
+        let parser = MusicXMLParser(store: store)
 
 
         let n0 = Note(value: .quarter, letter: .A, octave: 4)
-        let _ = part.insert(note: n0, intoMeasureIndex: 0, at: 1/4)
-
-        let parser = MusicXMLParser(store: store)
-
+        let _ = store.insert(note: n0, intoMeasureIndex: 0, at: 1/4)
 
         parser.save(filename: "test.xml")
     }
