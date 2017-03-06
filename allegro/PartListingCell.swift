21adaeead697aa1aabefc906ade50bef50142f8f
Nikhil Lele <nik.lele@gmail.com>
2017-03-06 15:41:17 -0800

    make ParListingCell attributes optionals

--- allegro/PartListingCell.swift
+++ allegro/PartListingCell.swift
@@ -13,44 +13,58 @@ import SwipyCell
 class PartListingCell: SwipyCell {
 
     static let reuseID = "PartListingCell"
-    static let height: CGFloat = 100
+    static let height: CGFloat = 80
+    static let margin: CGFloat = 3
 
-    let margin: CGFloat = 3
-
-    var filename: String = "" {
+    var filename: String? {
         didSet {
-            let partNumber: String = filename.components(separatedBy: "_").last ?? "X"
-            partTitle.text = part?.title ?? "Untitled Part \(partNumber)"
+            if let filename = filename {
+                let partNumber: String = filename.components(separatedBy: "_").last ?? "X"
+                partTitle.text = part?.title ?? "Untitled Part \(partNumber)"
+            } else {
+                partTitle.text = part?.title ?? "Untitled Part X"
+            }
         }
     }
 
     var part: Part? {
         didSet {
-            let partNumber: String = filename.components(separatedBy: "_").last ?? "X"
-            partTitle.text = part?.title ?? "Untitled Part \(partNumber)"
+            if let filename = filename {
+                let partNumber: String = filename.components(separatedBy: "_").last ?? "X"
+                partTitle.text = part?.title ?? "Untitled Part \(partNumber)"
+            } else {
+                partTitle.text = part?.title ?? "Untitled Part X"
+            }
         }
     }
 
-    var modified = Date() {
+    var modified: Date? {
         didSet {
-            date.text = Date.timeAgo(since: modified)
+            if let modified = modified {
+                date.text = Date.timeAgo(since: modified)
+            } else {
+                date.text = ""
+            }
         }
     }
 
     private let partTitle: UILabel = {
         let v = UILabel()
         v.font = UIFont(name: DEFAULT_FONT, size: 14)
+        v.backgroundColor = .white
         return v
     }()
 
     private let date: UILabel = {
         let v = UILabel()
-        v.font = UIFont(name: DEFAULT_FONT, size: 14)
+        v.font = UIFont(name: DEFAULT_FONT, size: 12)
+        v.backgroundColor = .white
         return v
     }()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .white
         addSubview(partTitle)
         addSubview(date)
     }
@@ -60,14 +74,20 @@ class PartListingCell: SwipyCell {
     }
 
     override func layoutSubviews() {
-        partTitle.frame = CGRect(x: margin,
-                                 y: margin,
-                                 width: bounds.width - 2 * margin,
-                                 height: bounds.height / 2)
-        date.frame = CGRect(x: margin,
+        partTitle.frame = CGRect(x: PartListingCell.margin,
+                                 y: PartListingCell.margin,
+                                 width: bounds.width - 2 * PartListingCell.margin,
+                                 height: bounds.height / 2 - 20)
+        date.frame = CGRect(x: PartListingCell.margin,
                             y: partTitle.frame.maxY,
-                            width: bounds.width - 2 * margin,
-                            height: bounds.height / 2)
+                            width: bounds.width - 2 * PartListingCell.margin,
+                            height: bounds.height / 2 - 20)
+    }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        filename = nil
+        part = nil
+        modified = nil
     }
 }
