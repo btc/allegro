baf86b2434fbeff5a97ec1d5add4b822c23ef4b2
Priyanka <ppsekhar@gmail.com>
2017-03-08 15:27:12 -0800

    audio playback determined based on duration rather than constant beat of 1

--- allegro/SideMenuViewController.swift
+++ allegro/SideMenuViewController.swift
@@ -221,7 +221,7 @@ class SideMenuViewController: UIViewController {
     }
     
     func playButtonTapped() {
-        audio?.playMeasure(part: store.part, measure: store.currentMeasure)
+        audio?.playFromCurrentMeasure(part: store.part, measure: store.currentMeasure)
         slideMenuController()?.closeRight()
     }
 
